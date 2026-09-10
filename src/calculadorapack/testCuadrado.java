6602691fcfb4d069ebf3a0385930888914ff73e8
dalimberAndrade <dalimberpubg@gmail.com>
2026-09-10 15:37:52 -0500

    Agregar dos objetos de cuadrado con sus respectivos calculos de area y perimetro

--- src/calculadorapack/testCuadrado.java
+++ src/calculadorapack/testCuadrado.java
@@ -13,6 +13,21 @@ public class testCuadrado {
 		double perimetroC1=c1.calcularPerimetro();
 		System.out.println("Perimetro cuadrado 1: "+perimetroC1);
 		
+		//SEGUNDO CUADRADO
+		Cuadrado c2=new Cuadrado();
+		c2.lado=3;
+		double areaC2=c2.calcularArea();
+		System.out.println("Area cuadrado 2:"+areaC2);
+		double perimetroC2=c2.calcularPerimetro();
+		System.out.println("Perimetro cuadrado 2: "+perimetroC2);
+		
+		//TERCER CUADRADO
+		Cuadrado c3=new Cuadrado();
+		c3.lado=6;
+		double areaC3=c3.calcularArea();
+		System.out.println("Area cuadrado 3:"+areaC3);
+		double perimetroC3=c3.calcularPerimetro();
+		System.out.println("Perimetro cuadrado 3: "+perimetroC3);
 
 	}
 
