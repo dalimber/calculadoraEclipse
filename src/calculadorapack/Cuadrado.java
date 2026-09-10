12a209f25adbddd7af306316fa4df0ba7dbb81b5
dalimberAndrade <dalimberpubg@gmail.com>
2026-09-10 15:13:56 -0500

    Crear clase Cuadrado, crear atributo lado, crear funcion calcular area

---
+++ src/calculadorapack/Cuadrado.java
@@ -0,0 +1,18 @@
+package calculadorapack;
+
+public class Cuadrado 
+{
+	//TALLER
+	public int lado;
+	
+	//CALCULAR AREA
+	public double calcularArea()
+	{
+		double area=lado*lado;
+		return area;
+	}
+	//CALCULAR PERIMETRO
+	
+	
+
+}
